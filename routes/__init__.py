908330a8982f275b70b04f0c2a2a32b239add2da
mycrmsystems <laurence.gould@hotmail.co.uk>
2025-08-19 18:36:49 +0100

    Update __init__.py

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,3 +1,2 @@
-# This file makes the directory a Python package
-# FILE 8 - Create this file in BOTH models/ and routes/ folders
-# This file can be completely empty - just needs to exist
+# Makes 'routes' a Python package.
+# This file can be empty.
